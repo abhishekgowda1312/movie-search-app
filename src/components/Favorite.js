ee1b88cb5cec242fb42cccbd727964e3d79cc0de
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2020-10-31 16:07:11 +0530

    final changes

---
+++ src/components/Favorite.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Favorite( {result,openPopup, toggleFavAction}) {
+    return (
+        <div className="result">
+        <img alt="imageH" src={result.Poster} onClick={() => openPopup(result.imdbID)}/>
+        <h3>{result.Title} <button type="button" onClick={() => toggleFavAction(result.imdbID)}>fav</button></h3>
+        <h6>Release date: {result.Year}</h6>
+    </div>
+    )
+}
+
+export default Favorite
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Result from './Result'
 
-function Results( { results, openPopup} ) {
+function Results( { results, openPopup, toggleFavAction} ) {
     
     return (
         <section className="results">
-           {results.map(result => <Result key={result.imdbID} result={result} openPopup={openPopup} />)}
+           {results.map(result => <Result key={result.imdbID} result={result} openPopup={openPopup} toggleFavAction={toggleFavAction}/>)}
         </section>
     )
 }
